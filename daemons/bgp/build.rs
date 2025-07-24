9ea5fbec1ba3df539c173952ba6da98eaea98974
Aytixel <aytixel@gmail.com>
2025-07-24 21:40:16 +0200

    Add basic dependencies

---
+++ daemons/bgp/build.rs
@@ -0,0 +1,14 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // clients
+    tonic_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .compile_protos(&["../../proto/kernel.proto"], &["../../proto"])?;
+
+    // server
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .compile_protos(&["../../proto/bgp.proto"], &["../../proto"])?;
+    Ok(())
+}
