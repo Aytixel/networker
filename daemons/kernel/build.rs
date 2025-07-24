ca4ed92c28b6864170db5c2714da1f2aeaac5fb2
Aytixel <aytixel@gmail.com>
2025-07-24 21:40:16 +0200

    Add basic dependencies

---
+++ daemons/kernel/build.rs
@@ -0,0 +1,10 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // clients
+
+    // server
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .compile_protos(&["../../proto/kernel.proto"], &["../../proto"])?;
+    Ok(())
+}
