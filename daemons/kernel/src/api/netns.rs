8cedbdef64e5a603e107ad827786b377852608a6
Aytixel <aytixel@gmail.com>
2025-08-09 19:47:23 +0200

    Add watch netns method

---
+++ daemons/kernel/src/api/netns.rs
@@ -0,0 +1,53 @@
+use std::sync::Arc;
+
+use futures::StreamExt;
+use tonic::Response;
+
+use crate::{
+    api::{Error, ResponseStream},
+    sys::netns::{Netns, NetnsWatcher, NetnsWatcherStream},
+};
+
+mod proto {
+    tonic::include_proto!("kernel.netns");
+}
+
+pub use proto::netns_service_server::NetnsServiceServer;
+
+#[derive(Clone)]
+pub struct NetnsService {
+    netns_watcher: Arc<NetnsWatcher>,
+}
+
+impl NetnsService {
+    pub fn new() -> Result<Self, Error> {
+        Ok(Self {
+            netns_watcher: NetnsWatcher::new()?,
+        })
+    }
+}
+
+#[tonic::async_trait]
+impl proto::netns_service_server::NetnsService for NetnsService {
+    type WatchNetnsStream = ResponseStream<proto::NetnsList>;
+
+    async fn watch_netns(
+        &self,
+        _request: tonic::Request<()>,
+    ) -> tonic::Result<tonic::Response<Self::WatchNetnsStream>> {
+        let netns_watcher_stream =
+            NetnsWatcherStream::new(self.netns_watcher.clone()).map(|list| {
+                Ok(proto::NetnsList {
+                    list: list
+                        .into_iter()
+                        .map(|netns| match netns {
+                            Netns::Default => proto::Netns { name: None },
+                            Netns::Named(name) => proto::Netns { name: Some(name) },
+                        })
+                        .collect(),
+                })
+            });
+
+        Ok(Response::new(Box::pin(netns_watcher_stream)))
+    }
+}
--- daemons/kernel/src/main.rs
+++ daemons/kernel/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod sys;
+
+use std::{net::SocketAddr, str::FromStr};
+
+use tonic::transport;
+use tracing_subscriber::FmtSubscriber;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Api(#[from] api::Error),
+    #[error(transparent)]
+    Transport(#[from] transport::Error),
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing::subscriber::set_global_default(
+        FmtSubscriber::builder()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_env(
+                "NETWORKER_KERNEL_LOG",
+            ))
+            .finish(),
+    )
+    .expect("tracing setup failed");
+
+    transport::Server::builder()
+        .add_service(api::NetnsServiceServer::new(api::NetnsService::new()?))
+        .serve(SocketAddr::from_str("[::1]:50051").unwrap())
+        .await?;
+
+    Ok(())
 }
