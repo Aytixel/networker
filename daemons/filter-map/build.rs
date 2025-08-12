ee5b56d28cffb7cdcbb15d36211468814030e0f4
Aytixel <aytixel@gmail.com>
2025-08-12 22:48:48 +0200

    Add filter-map daemon

--- daemons/filter-map/build.rs
+++ daemons/filter-map/build.rs
@@ -4,15 +4,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(false)
         .build_client(true)
         .compile_protos(&["../../proto/kernel.proto"], &["../../proto"])?;
-    tonic_build::configure()
-        .build_server(false)
-        .build_client(true)
-        .compile_protos(&["../../proto/filter-map.proto"], &["../../proto"])?;
 
     // server
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
-        .compile_protos(&["../../proto/bgp.proto"], &["../../proto"])?;
+        .compile_protos(&["../../proto/filter-map.proto"], &["../../proto"])?;
     Ok(())
 }
