54f98de56d7a27f2c1be7ab0a6843b358ffef55b
Aytixel <aytixel@gmail.com>
2025-11-09 10:23:50 +0100

    Rename filter-map to rib

--- daemons/bgp/build.rs
+++ daemons/bgp/build.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
-        .compile_protos(&["../../proto/filter-map.proto"], &["../../proto"])?;
+        .compile_protos(&["../../proto/rib.proto"], &["../../proto"])?;
 
     // server
     tonic_build::configure()
--- daemons/rib/build.rs
+++ daemons/rib/build.rs
@@ -9,6 +9,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
-        .compile_protos(&["../../proto/filter-map.proto"], &["../../proto"])?;
+        .compile_protos(&["../../proto/rib.proto"], &["../../proto"])?;
     Ok(())
 }
